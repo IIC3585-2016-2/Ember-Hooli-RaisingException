42dbf97b1ec7617379e5a62f3747c6643e36a376
vdrg <vrdragicevic@gmail.com>
2016-09-28 21:12:17 -0300

    Express server to ES6

--- express-api/routes/test.js
+++ express-api/routes/test.js
@@ -1,18 +1,19 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+const router = express.Router();
 
 
 // middleware that is specific to this router
-router.use(function timeLog(req, res, next) {
+router.use((req, res, next) => {
   console.log('Time: ', Date.now());
   next();
 });
+
 // define the home page route
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
   res.send('Birds home page');
 });
 // define the about route
-router.get('/about', function(req, res) {
+router.get('/about', (req, res) => {
   res.send('About birds');
 });
 
--- express-api/server.js
+++ express-api/server.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var request = require('request')
+import express from 'express';
+import mongoose from 'mongoose';
+import request from 'request';
 
-var app = express();
-var API_KEY = "9174426e1f8148708f5a0c653986e733"
+const app = express();
+const API_KEY = "9174426e1f8148708f5a0c653986e733"
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
   	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
@@ -14,7 +14,7 @@ app.use(function(req, res, next) {
 
 mongoose.connect('mongodb://localhost/emberData');
 
-var articleSchema = new mongoose.Schema({
+const articleSchema = new mongoose.Schema({
     source: 'string',
     author: "string",
     title: "string",
@@ -24,75 +24,70 @@ var articleSchema = new mongoose.Schema({
     publishedAt: "string"
 })
 
-var noteSchema = new mongoose.Schema({
+const noteSchema = new mongoose.Schema({
 	title: 'string',
 	content: 'string',
 	author: 'string'
 });
-var ArticleModel = mongoose.model('article',articleSchema)
-var NoteModel = mongoose.model('note',noteSchema);
+
+const ArticleModel = mongoose.model('article',articleSchema);
+const NoteModel = mongoose.model('note',noteSchema);
 //New lines!
-app.get('/api/',function(req,res) {
+app.get('/api/', (req,res) => {
 	res.send('Working');
 });
 
 
-app.get('/api/sources',function(req,res){
+app.get('/api/sources', (req,res) => {
   // Aqui deberia meter las cosas a moongose
   request.get("https://newsapi.org/v1/articles?source=techcrunch&apiKey=9174426e1f8148708f5a0c653986e733",
-  function(error,response,body){
-    if (!error && response.statusCode == 200){
-      var resp = JSON.parse(body)
-      var source = resp['source']
-      var articles = resp['articles']
-      for (elem in articles){
-        var article = articles[elem]
-        article.source = source;
-        console.log(article.author);
-        var _tmp = new ArticleModel(article)
-        _tmp.save(function (err) {
+    (error,response,body) => {
+      if (!error && response.statusCode == 200){
+        const resp = JSON.parse(body)
+        const source = resp['source']
+        const articles = resp['articles']
+        for (elem in articles) {
+          const article = articles[elem]
+          article.source = source;
+          console.log(article.author);
+          const _tmp = new ArticleModel(article)
+          _tmp.save(err => {
             if (err) {
               console.log(err);
             } else {
               console.log(article.author);
             }
           })
+        }
+        res.send(200)
+      } else {
+        res.send(300)
       }
-      res.send(200)
     }
-    else{
-      res.send(300)
-    }
-  }
-)
+  )
 });
 
-app.get('/api/articles',function(req,res){
-    ArticleModel.find({},function(err,docs) {
-		if(err) {
-			res.send(err);
-		}
-		else {
-      docs.sort(function(a,b){
+app.get('/api/articles', (req,res) => {
+  ArticleModel.find({}, (err,docs) => {
+    if(err) {
+      res.send(err);
+    } else {
+      docs.sort((a,b) => {
         a = new Date(a.publishedAt);
         b = new Date(b.publishedAt);
         return a>b ? -1 : a<b ? 1 : 0;
       });
-			res.send(docs);
-		}
-	});
+      res.send(docs);
+    }
+  });
 });
 
 
-app.get('/api/notes', function(req,res) {
-	NoteModel.find({},function(err,docs) {
-		if(err) {
-			res.send(err);
-		}
-		else {
-			res.send(docs);
-		}
-	});
+app.get('/api/notes', (req,res) => {
+  NoteModel.find({}, (err,docs) => {
+    if(err) res.send(err);
+    else res.send(docs);
+  });
 });
 
 app.listen('4500');
